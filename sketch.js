58bbbf71b809854f381dd0e37db72e0bfbfd7d11
laibasarwar <ls5118@nyu.edu>
2020-12-01 17:33:22 -0500

    Fixed Sound and Text FIle and Added Text Processing

--- sketch.js
+++ sketch.js
@@ -1,27 +1,37 @@
-function setup() {
-  createCanvas(windowWidth, windowHeight); 
+var dadatextfile;
+let font;
+fontsize=20;
+let lines = [];
+let phrase = 0;
+
+function setup() {  
+  createCanvas(windowWidth, windowHeight);  
+}  
+
+function preload(){
+  dadatextfile=loadStrings('dada_english.txt',doText);
+  font = loadFont('CormorantGaramond-Medium.ttf');
+}
+function draw() {  
+  background(0); 
+  textprocessed();
 }
 
-function draw() {
-  background(0);
-  rectMode(CORNER);
-  fill(255);
-  rect(width/2,height/2-350,600,700);
-  fill(255,0,0);
-  rect(width/2-600,height/2-350,600,700);
-  rectMode(CENTER);
-  fill(255,255,0);
-  rect(width/2-300,height/2,550,650);
-  fill(0);
-  rect(width/2+300,height/2-175,550,300);
-  fill(0,255,0);
-  rect(width/2+300,height/2+175,550,300);
-
-  for (let i=0; i<width; i+=50){
-      for (let j=0; j<height; j+=50){
-          stroke(0,0,255);
-          line(i,0,i,width);
-          line(0,j,1500,j);
-      }
+// Text Formatting //
+function doText(data) {
+  lines = data;
+}
+function textprocessed(){
+  for (var i = 0; i < lines.length; i++) {
+    fill(255); //textcolor white
+    textFont(font);
+    textSize(fontsize);
+    textAlign(CENTER);
+  // text(momwedding[i], 350, 25+i*12.5);
+    text(lines[phrase], windowWidth/2, windowHeight-50);
   }
 }
+function mousePressed() {
+  phrase++;
+}
+// End of Text Formatting //
