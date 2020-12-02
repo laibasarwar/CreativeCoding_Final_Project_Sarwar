309d1337a979fc6620b5c20688a8275edb13c414
laibasarwar <ls5118@nyu.edu>
2020-12-02 14:50:40 -0500

    Working Prototype - Added Comments
    I have issues with maps api

--- sketch.js
+++ sketch.js
@@ -1,70 +1,96 @@
-let scene=1;
+//working prototype - need help with maps api for third screen, will add title page in the first scene. I
+// took too much time on the content, but understanding the maps api was really hard since I had to use 
+//css and becuase of that it overlayed and did not play so I deleted it and are starting new. Sorry!
 
-var dadatextfile;
-let font;
-fontsize=20;
-let lines = [];
-let phrase = 0;
+let scene=1; //declaring scene to be 1
 
-var song;
-var amp;
-var volhistory = [];
+var dadatextfile; //declaring text file
+let font; //declaring font variable
+fontsize=20; //declaring size of font
+let lines = []; //declared lines array
+let phrase = 0; //declared first phrase of lines
 
-let imgparagraph=[];
+var song; //song variable
+var amp; //declare amplitude variable for song
+var volhistory = []; //declare an array to hold the amplitude
 
-var button;
+let imgparagraph=[];//array for the image paragraphs
 
-function toggleSong() {
-  if (song.isPlaying()) {
+var button;//button to toggle song
+
+function toggleSong() { //function to stop song through the button
+  if (song.isPlaying()) { 
     song.pause();
   } else {
     song.loop();
   }
 }
 
-function preload() {
-  song = loadSound('DadaUrdu3.mp3');
+function preload() { 
+  song = loadSound('DadaUrdu3.mp3'); //load voiceover in song variable
 
-  dadatextfile=loadStrings('dada_english.txt',doText);
-  font = loadFont('CormorantGaramond-Medium.ttf');
+  dadatextfile=loadStrings('dada_english.txt',doText); //import strings from text file into the doText function to transfer into data
+  font = loadFont('CormorantGaramond-Medium.ttf');//load selected font
 
-
-  imgparagraph[0]=loadImage('Journal1.png');
-  imgparagraph[1] = loadImage('Journal2.png');
-  imgparagraph[2] = loadImage('Journal3.png');
-
+  //did try to make this into a for loop when  doing it, it did not process while I was running it. 
+  imgparagraph[0]=loadImage('Journal0.png');//load images in the array
+  imgparagraph[1]=loadImage('Journal1.png');
+  imgparagraph[2] = loadImage('Journal2.png');
+  // imgparagraph[3] = loadImage('Journal3.png');
+  // imgparagraph[4] = loadImage('Journal4.png');
+  // imgparagraph[5] = loadImage('Journal5.png');
+  // imgparagraph[6] = loadImage('Journal6.png');
+  // imgparagraph[7] = loadImage('Journal7.png');
+  // imgparagraph[8] = loadImage('Journal8.png');
+  // imgparagraph[9] = loadImage('Journal9.png');
+  // imgparagraph[10] = loadImage('Journal10.png');
+  // imgparagraph[11] = loadImage('Journal11.png');
+  // imgparagraph[12] = loadImage('Journal12.png');
+  // imgparagraph[13] = loadImage('Journal13.png');
+  // imgparagraph[14] = loadImage('Journal14.png');
+  // imgparagraph[15] = loadImage('Journal5.png');
+  // imgparagraph[16] = loadImage('Journal6.png');
+  // imgparagraph[17] = loadImage('Journal7.png');
+  // imgparagraph[18] = loadImage('Journal8.png');
+  // imgparagraph[19] = loadImage('Journal9.png');
+  // imgparagraph[20] = loadImage('Journa20.png');
+  // imgparagraph[21] = loadImage('Journal21.png');
+  // imgparagraph[22] = loadImage('Journal22.png');
+  // imgparagraph[23] = loadImage('Journal23.png');
+  // imgparagraph[24] = loadImage('Journal24.png');
+  // imgparagraph[25] = loadImage('Journal25.png');
 }
 
 function setup() {
-  createCanvas(windowWidth, windowHeight);
+  createCanvas(windowWidth, windowHeight);//full screen
   if (scene==1){
-    button = createButton('toggle');
-    button.position(windowWidth/2-20,100);
-    button.mousePressed(toggleSong);
-    song.play();
-    amp = new p5.Amplitude();
+    button = createButton('press to on and off the song');//button to pause and play song
+    button.position(windowWidth/2-20,100);//position to be in top middle
+    button.mousePressed(toggleSong);//enable by mousePress
+    song.play();//play song function
+    amp = new p5.Amplitude();//creat new amplitude class from built in p5 sound library
   }
 }
 
 function draw() {
   if (scene==1){
-    background(161,191,157,75);
-    drawSquare(); 
-    textprocessed();
-    soundWave(); 
-    writing();
-    reading();
+    background(161,191,157,75);//green background
+    drawSquare(); //function to make frame
+    textprocessed();//function for text
+    soundWave(); //function for sound wave
+    writing(); //wanted to make a person writing on paper to illusterate grandfather, the rectangle is a desk for now -- i might not do this because I am more focusing on the map api now
+    reading();//do the same for person reading the journal(my brother)
   } else if (scene==2){
-    background(161,191,157,75);
-    button.position(windowWidth/2,windowHeight); 
+    background(161,191,157,75);//green background
+    button.position(windowWidth/2,windowHeight); //take out button from view
     drawSquare();  
     textprocessed();
     soundWave();
-    imgChange();
+    imgChange();//call to function for images array 
   } else if (scene==3){
     background(161,191,157,75);
     button.position(windowWidth/2,windowHeight);
-    initMap();
+    initMap();//google map api function but it was not working so I am trying to change it to the leaflet libray but it is giving me the same issue
   }
   
 
@@ -72,19 +98,19 @@ function draw() {
 }
 
 function doText(data) {
-  lines = data;
+  lines = data;//puts the lines array to the data to use as the text
 }
 
 function drawSquare(){
-  noStroke();
+  noStroke();//white squares to make a frame on each side of the window
   fill(255);
-  rect(0,windowHeight-100,windowWidth,100);
+  rect(0,windowHeight-100,windowWidth,100);//I am using windowWidth to make it more responsive and not having to completely hardcode
   rect(0,0,windowWidth,100);
   rect(0,0,100,windowHeight);
   rect(windowWidth-100,0,100,windowHeight);
 }
 function textprocessed(){
-  for (var i = 0; i < lines.length; i++) {
+  for (var i = 0; i < lines.length; i++) {//processed the text file line by line to add as subtitles
     stroke(0);
     fill(0); //textcolor white
     textFont(font);
@@ -93,33 +119,33 @@ function textprocessed(){
     text(lines[phrase], windowWidth/2, windowHeight-50);
   }
 }
-function mousePressed() {
+function mousePressed() {//when mouse is pressed, the phrase will go up -- I will chnage this to a set time using millis later
   phrase++;
 }
 
-function soundWave(){
-  var vol = amp.getLevel();
-  volhistory.push(vol);
-  var currentY = map(vol, 1, 0, height, 1);
-  translate(0, y);
+function soundWave(){//adapted from https://www.youtube.com/watch?v=jEwAMgcCgOA
+  var vol = amp.getLevel();//gets amp level and stors it to vol
+  volhistory.push(vol);//puts that vol level into the array
+  var currentY = map(vol, 1, 0, height, 1);//make it so that it is positioned on to top portion of the screen, I want to find a way to make it more colourful though
+  translate(0, y);//translates it to y 
 
   beginShape();
   stroke(0);
-  for (var i = 0; i < volhistory.length; i++) {
-    var y = map(volhistory[i], 1, 0, height, 1);
-    vertex(i, y);
-    fill(vol,0);
+  for (var i = 0; i < volhistory.length; i++) {//accessing each amp volume in song file and array
+    var y = map(volhistory[i], 1, 0, height, 1);//puts it into each amp level and positions it on top using height
+    vertex(i, y);//each vertex from the translation of y
+    fill(vol,0);//fill the level from vol variable
   }
   endShape();
-  if (volhistory.length > width) {
-    volhistory.splice(0, 1);
-  line(width-1,0,width-1,1);
+  if (volhistory.length > width) {//to make sure that when the track goes to the end of the screen, the line moves
+    volhistory.splice(0, 1);//removes the value from the array
+  line(width-1,0,width-1,1);//creates the line overall and positioned on top 
   }
 }
 
-function writing(){
+function writing(){//may not include
   noFill();
-  stroke(0);
+  stroke(0);//make rectangle for writing table
   let centerx=windowWidth/2;
   let centery=windowHeight/2;
   console.log(centerx);
@@ -137,7 +163,7 @@ function writing(){
 
 function reading(){
 
-  noFill();
+  noFill();//make rectangle for reading the journal
   stroke(0);
   let centerx=windowWidth/2;
   let centery=windowHeight/2;
@@ -155,23 +181,23 @@ function reading(){
 }
 
 function imgChange(){
-  for(i = 0; i < imgparagraph.length; i++){
-    imageMode(CENTER);
-    image(imgparagraph[i], random(100,windowWidth-150), random(100,windowHeight-150), 200,100);
+  for(i = 0; i < imgparagraph.length; i++){//parses the imgparagraph array 
+    imageMode(CENTER);//centers position
+    image(imgparagraph[i], random(100,windowWidth-150), random(100,windowHeight-150), 200,100);//places the image in array in random placess inside the frame with size of 200,100
   }
 }
 
-function initMap(){
-  map = new google.maps.Map(document.getElementById("map"), {
-    center: { lat: 45.518, lng: -122.672 },
-    zoom: 18,
+function initMap(){//from maps api but I might remove to use leaflet instead
+  map = new google.maps.Map(document.getElementById("map"), {//deleted this from index file but will try to make it work
+    center: { lat: 45.518, lng: -122.672 },//centers latitude and longitude of initial position of map
+    zoom: 18,//max zoom in
   });
 }
 
 function keyPressed(){
   if (keyCode==32){
-    scene++;
+    scene++;//spacebar moves scene
     if (choice>3){}
-    scene=1;
+    scene=1;//repositions to 1rst scene if space exaceeds the number of scenes
   }
 }
