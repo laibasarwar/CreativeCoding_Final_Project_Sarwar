973caf19b511aebdad434f998c238418e91851fd
laibasarwar <ls5118@nyu.edu>
2020-12-01 23:43:46 -0500

    Added Music Wave and Trying to Make the Google Maps API work

--- sketch.js
+++ sketch.js
@@ -1,37 +1,149 @@
+let scene=1;
+
 var dadatextfile;
 let font;
 fontsize=20;
 let lines = [];
 let phrase = 0;
 
-function setup() {  
-  createCanvas(windowWidth, windowHeight);  
-}  
+var song;
+var amp;
+var volhistory = [];
 
-function preload(){
+var button;
+
+function toggleSong() {
+  if (song.isPlaying()) {
+    song.pause();
+  } else {
+    song.loop();
+  }
+}
+
+function preload() {
+  song = loadSound('DadaUrdu3.mp3');
+
   dadatextfile=loadStrings('dada_english.txt',doText);
   font = loadFont('CormorantGaramond-Medium.ttf');
 }
-function draw() {  
-  background(0); 
-  textprocessed();
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  button = createButton('toggle');
+  button.position(windowWidth/2-20,100);
+  button.mousePressed(toggleSong);
+  song.play();
+  amp = new p5.Amplitude();
 }
 
-// Text Formatting //
+function draw() {
+  background(161,191,157,75);
+  drawSquare();  
+  textprocessed();
+  // soundWave();
+  writing();
+  reading();
+
+}
+
 function doText(data) {
   lines = data;
 }
+
+function drawSquare(){
+  noStroke();
+  fill(255);
+  rect(0,windowHeight-100,windowWidth,100);
+  rect(0,0,windowWidth,100);
+  rect(0,0,100,windowHeight);
+  rect(windowWidth-100,0,100,windowHeight);
+}
 function textprocessed(){
   for (var i = 0; i < lines.length; i++) {
-    fill(255); //textcolor white
+    stroke(0);
+    fill(0); //textcolor white
     textFont(font);
     textSize(fontsize);
     textAlign(CENTER);
-  // text(momwedding[i], 350, 25+i*12.5);
     text(lines[phrase], windowWidth/2, windowHeight-50);
   }
 }
 function mousePressed() {
   phrase++;
 }
-// End of Text Formatting //
+
+function soundWave(){
+  var vol = amp.getLevel();
+  volhistory.push(vol);
+  var currentY = map(vol, 1, 0, height, 1);
+  translate(0, y);
+
+  beginShape();
+  stroke(0);
+  for (var i = 0; i < volhistory.length; i++) {
+    var y = map(volhistory[i], 1, 0, height, 1);
+    vertex(i, y);
+    fill(vol,0);
+  }
+  endShape();
+  if (volhistory.length > width) {
+    volhistory.splice(0, 1);
+  line(width-1,0,width-1,1);
+  }
+}
+
+function writing(){
+  noFill();
+  stroke(0);
+  let centerx=windowWidth/2;
+  let centery=windowHeight/2;
+  console.log(centerx);
+  console.log(centery);
+
+  push();
+  beginShape();
+
+  rectMode(CENTER);
+  rect(centerx+40,centery,80,100);
+  
+  endShape();
+  pop();
+
+
+
+}
+
+function reading(){
+
+  noFill();
+  stroke(0);
+  let centerx=windowWidth/2;
+  let centery=windowHeight/2;
+  console.log(centerx);
+  console.log(centery);
+
+  push();
+  beginShape();
+
+  rectMode(CENTER);
+  rect(centerx-40,centery,80,100);
+
+  endShape();
+  pop();
+}
+
+function initMap(){
+  var options = {
+    zoom:8,
+    center:8,
+    center:{lat:42.3601,lng:-71.0580}
+  }
+}
+
+function keyPressed(){
+  if (keyCode==32){
+    scene++;
+    if (choice>3){}
+    scene=1;
+  }
+}
