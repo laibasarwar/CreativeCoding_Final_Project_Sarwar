a180d6c2d10e66bd2836a7bfc86be57ff72bd856
laibasarwar <ls5118@nyu.edu>
2020-12-04 23:47:15 -0500

    Trying to make train move with vectors but first converting to class and organizing

--- sketch.js
+++ sketch.js
@@ -1,6 +1,39 @@
 
 //references for map: https://www.youtube.com/watch?v=Ae73YY_GAU8&ab_channel=TheCodingTrain, https://mappa.js.org/docs/using-data.html
 
+
+class Destination {
+  constructor(x, y, diameter, name) {
+    this.x = x;
+    this.y = y;
+    this.diameter = diameter;
+    this.radius = diameter / 2;
+    this.name = name;
+
+    this.over = false;
+  }
+
+  // Check if mouse is over the destination
+  rollover(px, py) {
+    let d = dist(px, py, this.x, this.y);
+    this.over = d < this.radius;
+  }
+
+  // Display the destination
+  display() {
+    stroke(0);
+    strokeWeight(0.8);
+    noFill();
+    imageMode(CENTER);
+    image(pin,this.x,this.y,20,30);
+    if (this.over) {
+      fill(0);
+      textAlign(CENTER);
+      text(this.name+" "+this.x+" "+this.y, this.x, this.y + this.radius + 20);
+    }
+  }
+}
+
 let scene=1; //declaring scene to be 1
 
 var button;//button to toggle song
@@ -19,10 +52,16 @@ let imgparagraph=[];//array for the image paragraphs
 
 let dadadestinationsdata; //
 
+let destination1;
+let velocity;
+let destination2;
+
 const mappa = new Mappa('Leaflet');//creates new map class with the leaflet library
 let pakistanmap;//declared variable for my map
 let canvas;//declares canvas variable to declare the overall canvas
 
+let latitude;
+let longitude;
 
 const options = {//options for initial position of map
   lat: 30.3753,//initial latitude
@@ -36,6 +75,7 @@ function toggleSong() { //function to stop song through the button
     song.pause();
   } else {
     song.loop();
+    // song.play();//play song function
   }
 }
 
@@ -77,23 +117,31 @@ function preload() {
   // imgparagraph[23] = loadImage('Journal23.png');
   // imgparagraph[24] = loadImage('Journal24.png');
   // imgparagraph[25] = loadImage('Journal25.png');
+  pin=loadImage("https://cdn0.iconfinder.com/data/icons/small-n-flat/24/678111-map-marker-512.png");
+  partitiontrain=loadImage('train.png');
 }
 
 function setup() {
   canvas = createCanvas(windowWidth, windowHeight);//variable for canvas
   pakistanmap = mappa.tileMap(options);//initializes the map with the initial options variable
   pakistanmap.overlay(canvas);//overlays the canvas == very important
+  let latitude;
+  let longitude;
+  // destination1=createVector(latitude,logitude);
+
+
   
     button = createButton('press to on and off the song');//button to pause and play song
     button.position(windowWidth/2-20,100);//position to be in top middle
     button.mousePressed(toggleSong);//enable by mousePress
-    // song.play();//play song function
+    song.play();//play song function
     amp = new p5.Amplitude();//creat new amplitude class from built in p5 sound library
 }
 
 function draw() {
   if (scene==1){
     background(161,191,157,75);//green background
+    // button.position(windowWidth/2-20,100);//position to be in top middle
     drawSquare(); //function to make frame
     textprocessed();//function for text
     soundWave(); //function for sound wave
@@ -112,23 +160,27 @@ function draw() {
     pakistanmap.overlay(canvas);//overlays the canvas with the map
     for (let i = 0; i < dadadestinationsdata.getRowCount(); i++) {//goes through each row in the csv file
       
-      const latitude = Number(dadadestinationsdata.getString(i, 'geometry/coordinates/1'));//lat string from csv into the number
-      const longitude = Number(dadadestinationsdata.getString(i, 'geometry/coordinates/0'));//long string from csv into #
+      latitude = Number(dadadestinationsdata.getString(i, 'geometry/coordinates/1'));//lat string from csv into the number
+      longitude = Number(dadadestinationsdata.getString(i, 'geometry/coordinates/0'));//long string from csv into #
 
-      const pos = pakistanmap.latLngToPixel(latitude, longitude);//puts the latitude and longitude in the pos variable 
-      ellipse(pos.x, pos.y, 25);//ellipse for each destination -- will turn into maps pin png
+      let pos = pakistanmap.latLngToPixel(latitude, longitude);//puts the latitude and longitude in the pos variable 
+      // ellipse(pos.x, pos.y, 25);//ellipse for each destination -- will turn into maps pin png
       // var marker = L.marker([pos.x, pos.y]).addTo(mymap); == doesnt work
       // ellipse(pos.x[0],pos.y[0],200);
-      
+      destination1=pos
+      image(mappin,pos.x,pos.y,30,53);
+
       const train=pakistanmap.latLngToPixel(31.604757,74.574136);//for the train == 
-      ellipse(train.x,train.y,50);//will change to train png
+      // ellipse(train.x,train.y,50);//will change to train png
+      image(partitiontrain, train.x,train.y,80,72);
+      
+      
+      // destination1=createVector(train.x,train.y);
+      // velocity=createVector(0,1);
+      // destination1.add(velocity);
+      // ellipse(destination.x, destination.y, 32);
     } 
-    
-}
-  
-  
-
-
+  }  
 }
 function doText(data) {
   lines = data;//puts the lines array to the data to use as the text
@@ -160,7 +212,7 @@ function soundWave(){//adapted from https://www.youtube.com/watch?v=jEwAMgcCgOA
   var vol = amp.getLevel();//gets amp level and stors it to vol
   volhistory.push(vol);//puts that vol level into the array
   var currentY = map(vol, 1, 0, height, 1);//make it so that it is positioned on to top portion of the screen, I want to find a way to make it more colourful though
-  translate(0, y);//translates it to y 
+  translate(width/2, y);//translates it to y 
 
   beginShape();
   stroke(0);
@@ -225,7 +277,7 @@ function imgChange(){
 function keyPressed(){
   if (keyCode==32){
     scene++;//spacebar moves scene
-    if (scene>3){
+    if (scene>4){
       scene=1;//repositions to 1rst scene if space exaceeds the number of scenes
     }
     
