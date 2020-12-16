f7b2cfb4f96d3cce0f4ffd85cb513ac922e9774a
laibasarwar <ls5118@nyu.edu>
2020-12-16 11:58:38 -0500

    Revised Code

--- sketch.js
+++ sketch.js
@@ -5,7 +5,7 @@ let dix=50;
 let diy=50;
 let pos;
 
-let scene=1; //declaring scene to be 1
+let scene=0; //declaring scene to be 1
 
 var button;//button to toggle song
 
@@ -15,6 +15,8 @@ let fonttitle;
 fontsize=20; //declaring size of font
 let lines = []; //declared lines array
 let phrase = 0; //declared first phrase of lines
+
+let word=0;
 
 var song; //song variable
 var amp; //declare amplitude variable for song
@@ -32,7 +34,7 @@ let r=255;
 let g=0;
 let b=0;
 let ximg,yimg,ximg1,ximg2;
-let squareWidth,squareWidth1,squareWidth2,squareWidth3,squareWidth4,squareWidth5;
+let squareWidth,squareWidth1,squareWidth2,squareWidth3,squareWidth4,squareWidth5,transwidth, transwidth2;
 let v,vx,vy;
 
 const mappa = new Mappa('Leaflet');//creates new map class with the leaflet library
@@ -44,6 +46,7 @@ let pindi_home;
 let pindix=0;
 let pindiy=0;
 
+let series0,series1,series2,series3,series4,dadaimage;
 
 const options = {//options for initial position of map
   lat: 30.9119721,//initial latitude
@@ -71,6 +74,7 @@ function preload() {
   fonttitle=loadFont('partner.otf');
   cormorant=loadFont('CormorantGaramond-Medium.ttf');
   comforta=loadFont('Comfortaa-VariableFont_wght.ttf');
+  
 
 
   
@@ -84,8 +88,13 @@ function preload() {
   partitiontrain=loadImage('train.png');
   mapimage=loadImage('mapimage.png');
   pindi_home=loadImage('pindi_home.png');
-  
-
+  orangegreen=loadImage('orange_green_4.png');
+  series0=loadImage('series0.jpg');
+  series1=loadImage('series1.jpg');
+  series2=loadImage('series2.jpg');
+  series3=loadImage('series3.jpg');
+  series4=loadImage('series4.jpg');
+  dadaimage=loadImage('dada2.jpg');
 }
 
 
@@ -113,7 +122,7 @@ function setup() {
     // button = createButton('on/off');//button to pause and play song
     // button.position(windowWidth/2-25,100);//position to be in top middle
     // button.mousePressed(toggleSong);//enable by mousePress
-    // song.play();//play song function
+    song.play();//play song function
     amp = new p5.Amplitude();//creat new amplitude class from built in p5 sound library    
     setInterval(movingPhrases,5000);
 
@@ -127,6 +136,8 @@ function setup() {
     squareWidth3=0;
     squareWidth4=0;
     squareWidth5=0;
+    transwidth=0;
+    transwidth2=0;
     v=0;
     vx=0;
     vy=0;
@@ -134,9 +145,417 @@ function setup() {
 }
 
 function draw() {
+  push();
+  let partitiontitle=["THE","PARTITION","OF","INDIA","AND","PAKISTAN","IN","1947","WAS A","BRUTAL","AND","DEADLY","EVENT"];
+  let partitionsum=["UP","TO","2","MILLION","PEOPLE DIED,","AND","ABOUT","14","MILLION","PEOPLE","WERE","DISPLACED"];
+  let partitionsum2=["MY","GRANDFATHER","WAS","ONE","OF","THE","LUCKY","FEW","WHO","WAS","ABLE","TO","ESCAPE","INDIA","AND","GO","TO","PAKISTAN"];
+  let partiotionsum3=["THIS","IS","HIS","STORY"];
+
+  textFont(cormorant);
+  textAlign(CENTER);
+  if (millis()>= 0) {
+    
+    image(orangegreen,0,0,windowWidth,windowHeight);
+
+    textFont(cormorant);
+    textStyle(BOLD);
+    textAlign(CENTER);
+    textSize(50);
+    fill(255);
+    stroke(255);
+    strokeWeight(5);
+    
+    text(partitiontitle[0],width/2,(height/2)-300);
+    
+  } 
+  if (millis()>=2000){
+    text(partitiontitle[1],width/2,height/2-250);
+  }
+  if (millis()>=3000){
+    text(partitiontitle[2],width/2,height/2-200);
+  }
+  if (millis()>=4000){
+    text(partitiontitle[3],width/2+150,height/2-150);
+  }
+  if (millis()>=5000){
+    text(partitiontitle[4],width/2,height/2-100);
+  }
+  if (millis()>=6000){
+    text(partitiontitle[5],width/2-150,height/2-150);
+  }
+  if (millis()>=7000){
+    text(partitiontitle[6],width/2,height/2-50);
+  }
+  if (millis()>=8000){
+    text(partitiontitle[7],width/2,height/2);
+  }
+  if (millis()>=9000){
+    text(partitiontitle[8],width/2,height/2+50);
+  }
+  if (millis()>=10000){
+    text(partitiontitle[9],width/2,height/2+100);
+  }
+  if (millis()>=11000){
+    text(partitiontitle[10],width/2,height/2+150);
+  }
+  if (millis()>=12000){
+    text(partitiontitle[11],width/2,height/2+200);
+  }
+  if (millis()>=13000){
+    text(partitiontitle[12],width/2,height/2+250);
+  }
+  if (millis()>=14000){
+    text(partitiontitle[13],width/2,height/2+300);
+  }
+  pop();
+  push();
+  
+  if (millis()>=15000){
+    fill(217,120,85);
+    noStroke();
+    rect(width/2,0,transwidth,height);
+    // transwidth=transwidth+(200);
+    transwidth=transwidth+10;
+    if (transwidth>windowWidth/2){
+      transwidth=windowWidth/2;
+    }
+    
+    fill(161,191,157);
+    rect(width/2,0,transwidth2,height);
+    // transwidth=transwidth+(200);
+    transwidth2=transwidth2-10;
+    if (transwidth2<-(windowWidth/2)){
+      transwidth2=-(windowWidth/2);
+    }
+  }
+  pop();
+
+  if (millis()>=18000){
+    fill(217,120,85);
+    noStroke();
+    rect(width/2,0,width/2,height);
+    fill(161,191,157);
+    rect(0,0,width/2,height);
+
+    push();
+    
+    noStroke(0);
+    strokeWeight(1);
+    fill(255);
+    rectMode(CENTER);
+    rect(width/3-200,height/3+100,400,400);
+    imageMode(CENTER);
+    image(series0,width/3-175,height/3+125,400,400);
+    // pop();
+    noStroke(0);
+
+    textFont(cormorant);
+    textStyle(BOLD);
+    textAlign(CENTER);
+    textSize(50);
+    fill(255);
+    stroke(255);
+
+    fill(255);
+    text(partitionsum[0],width/2+(width/4),height/2-250);
+  }
+  
+  if (millis()>=19000){
+    text(partitionsum[1],width/2+(width/4),height/2-200);
+    
+    
+  }
+  if (millis()>=20000){
+    fill(255);
+    text(partitionsum[2],width/2+(width/4),height/2-150);
+  }
+  if (millis()>=21000){
+    fill(255);
+    text(partitionsum[3],width/2+(width/4),height/2-100);
+    image(series1,width/3-175,height/3+125,400,400);
+  }
+  if (millis()>=22000){
+    fill(255);
+    text(partitionsum[4],width/2+(width/4),height/2-50);
+  }
+  if (millis()>=23000){
+    fill(255);
+    text(partitionsum[5],width/2+(width/4),height/2);
+    image(series2,width/3-175,height/3+125,400,400);
+  }
+  if (millis()>=24000){
+    fill(255);
+    text(partitionsum[6],width/2+(width/4),height/2+50);
+  }
+  if (millis()>=25000){
+    fill(255);
+    text(partitionsum[7],width/2+(width/4),height/2+100);
+    image(series3,width/3-175,height/3+125,400,400);
+  }
+  if (millis()>=26000){
+    fill(255);
+    text(partitionsum[8],width/2+(width/4),height/2+150);
+  }
+  if (millis()>=27000){
+    fill(255);
+    text(partitionsum[9],width/2+(width/4),height/2+200);
+    image(series4,width/3-175,height/3+125,400,400);
+  }
+  if (millis()>=28000){
+    fill(255);
+    text(partitionsum[10],width/2+(width/4),height/2+250);
+  }
+  if (millis()>=29000){
+    fill(255);
+    // text(partitionsum[11],width/2+(width/4),300);
+    text(partitionsum[11],random(width/2,width),random(0,height));
+  }
+  pop();
+  push();
+  if (millis()>=30000){
+
+    fill(217,120,85);
+    noStroke();
+    rect(width/2,0,width/2,height);
+    fill(161,191,157);
+    rect(0,0,width/2,height);
+
+    push();
+    noStroke(0);
+    strokeWeight(1);
+    fill(255);
+    rectMode(CENTER);
+    rect(width/2+width/4,height/2,400,400);
+    imageMode(CENTER);
+    image(dadaimage,width/2+width/4-25,height/2-25,400,400);
+    textFont(cormorant);
+    textStyle(BOLD);
+    textAlign(CENTER);
+    textSize(50);
+    fill(255);
+    stroke(255);
+    // pop();
+    noStroke(0);
+
+    fill(255);
+    // text(partitionsum[11],width/2+(width/4),300);
+    text(partitionsum2[0],width/2-(width/4),height/2-250);
+  }
+
+  if (millis()>=30000){
+    text(partitionsum2[1],width/2-(width/4),height/2-200);
+  }
+  if (millis()>=31000){
+    text(partitionsum2[2],width/2-(width/4),height/2-150);
+  }
+  if (millis()>=32000){
+    text(partitionsum2[3],width/2-(width/4)-100,height/2-100);
+    text(partitionsum2[4],width/2-(width/4),height/2-100);
+    text(partitionsum2[5],width/2-(width/4)+100,height/2-100);
+  }
+  if (millis()>=33000){
+    text(partitionsum2[6],width/2-(width/4),height/2-50);
+  }
+  if (millis()>=34000){
+    text(partitionsum2[7],width/2-(width/4),height/2);
+  }
+  if (millis()>=35000){
+    text(partitionsum2[8],width/2-(width/4)-150,height/2+50);
+    text(partitionsum2[9],width/2-(width/4),height/2+50);
+    text(partitionsum2[10],width/2-(width/4)+150,height/2+50);
+  }
+  if (millis()>=36000){
+    text(partitionsum2[11],width/2-(width/4)-50,height/2+100);
+    text(partitionsum2[12],width/2-(width/4)+50,height/2+100);
+  }
+  if (millis()>=37000){
+    text(partitionsum2[13],width/2-(width/4),height/2+150);
+  }
+  if (millis()>=38000){
+    text(partitionsum2[14],width/2-(width/4)-1,height/2+200);
+    text(partitionsum2[15],width/2-(width/4),height/2+200);
+    text(partitionsum2[16],width/2-(width/4)+150,height/2+200);
+  }
+  if (millis()>=39000){
+    text(partitionsum2[17],width/2-(width/4),height/2+250);
+  }
+  
+  if (millis()>=40000){
+    text(partitionsum2[17],width/2-(width/4),height/2+250);
+  }
+  pop();
+  
+  strokeWeight(0);
+
+  push();
+  if (millis()>=41000){
+    background(255);//green background
+    //position to be in top middle
+    // song.play();
+    drawSquare(); //function to make frame
+    textprocessed();//function for text
+    soundWave(); //function for sound wave
+    // button.position(windowWidth/2-25,100);
+  }
+  pop();
+  push();
+    if (millis()>=41000){
+      background(255);//green background
+    
+      song.pause();
+      // button.position(windowWidth/2,windowHeight); //take out button from view
+      textFont(fonttitle);
+      textAlign(CENTER);
+      textSize(35);
+      let title2="Journal Entries of My Grandfather's Train Journey";
+      text(title2,windowWidth/2,150);
+      imgChange();//call to function for images array 
+      drawSquare();  
+    }
+  // pop();
+  push();
+    if (millis()>=42000){
+      ong.pause();
+    background(255);
+    clear();//basically new background, but for map specifically
+    pakistanmap.overlay(canvas);//overlays the canvas with the map
+
+    for (let i = 0; i < destinations.length; i++) {
+      destinations[i].display();
+    }  
+    
+    let train0=pakistanmap.latLngToPixel(28.5885595,77.2549491); //Hazrat Station
+    let t0=createVector(train0.x,train0.y);
+    
+    let train1=pakistanmap.latLngToPixel(28.6428915,77.2190894); //New Delhi Station
+    let t1=createVector(train1.x,train1.y);
+    stroke(0);
+    drawTrain(t0,t1);
+
+    let train2=pakistanmap.latLngToPixel(30.9119721,75.8495208); //Ludhiana Station
+    let t2=createVector(train2.x,train2.y); 
+    stroke(0);
+    drawTrain(t1,t2);
+
+    let train3=pakistanmap.latLngToPixel(31.6332336,74.8672281); //Amritsar Station
+    let t3=createVector(train3.x,train3.y); 
+    stroke(0);
+    drawTrain(t2,t3);
+
+    let train4=pakistanmap.latLngToPixel(31.6047570,74.5741360); //Wagah Border
+    let t4=createVector(train4.x,train4.y);
+    stroke(0);
+    drawTrain(t3,t4);
+
+    noStroke();
+
+    if ((mouseX>train0.x-(dix/2))&& (mouseX<train0.x+(dix/2)) && (mouseY>train0.y-(diy/2)) && (mouseY<train0.y+(diy/2))){//boolean if mousex and mousey is in each cardd
+      fill(217,120,85);
+      rect((train0.x+20),train0.y,290,150,25);
+      stroke(0);
+      fill(0);
+
+      let station0="Hazrat Nizamuddin Railway Station\n- India -\nSeptember 19, 2020\nInitial Boarding Station For Pakistan\n- found a small spot cabin space to sit in\n- with a friend and his family\n- constantly searched for weaponry";
+      textAlign(CENTER);
+      textFont(comforta);
+      textLeading(20);
+      text(station0,train0.x+165,train0.y+20);
+    }
+    
+    if ((mouseX>train1.x-(dix/2))&& (mouseX<train1.x+(dix/2)) && (mouseY>train1.y-(diy/2)) && (mouseY<train1.y+(diy/2))){//boolean if mousex and mousey is in each cardd
+      fill(217,120,85);
+      rect((train1.x+20),train1.y,290,140,25);
+      stroke(0);
+      fill(0);
+
+      let station1="- New Delhi Railway Station -\n- India -\nSeptember 19, 1947,\nFirst Train Stop\n- saw armed Hindus and Sikhs\n- heard gunfire from the City";
+      textAlign(CENTER);
+      textFont(comforta);
+      textLeading(20);
+      text(station1,train1.x+165,train1.y+20);
+    }
+
+
+    if ((mouseX>train2.x-(dix/2))&& (mouseX<train2.x+(dix/2)) && (mouseY>train2.y-(diy/2)) && (mouseY<train2.y+(diy/2))){//boolean if mousex and mousey is in each cardd
+      fill(217,120,85);
+      rect((train2.x+20),train2.y,290,175,25);
+      stroke(0);
+      fill(0);
+
+      let station2="- Ludhiana Railway Station -\n- India -\nSeptember 20, 1947\nSecond Train Stop\n- carts filled with holes\n- about 100 people martyred\n- women and children missing\n- about 40-50 people survived";
+      textAlign(CENTER);
+      textFont(comforta);
+      textLeading(20);
+      text(station2,train2.x+165,train2.y+20);
+    }
+
+
+
+    if ((mouseX>train3.x-(dix/2))&& (mouseX<train3.x+(dix/2)) && (mouseY>train3.y-(diy/2)) && (mouseY<train3.y+(diy/2))){//boolean if mousex and mousey is in each cardd
+      fill(217,120,85);
+      rect((train3.x+20),train3.y,290,100,25);
+      stroke(0);
+      fill(0);
+
+      let station3="- Amritsar Junction -\n- India -\nSeptember 20, 1947\nThird Train Stop";
+      textAlign(CENTER);
+      textFont(comforta);
+      textLeading(20);
+      text(station3,train3.x+165,train3.y+20);
+    }
+
+
+    if ((mouseX>train4.x-(dix/2))&& (mouseX<train4.x+(dix/2)) && (mouseY>train4.y-(diy/2)) && (mouseY<train4.y+(diy/2))){//boolean if mousex and mousey is in each cardd
+      fill(161,191,157);
+      rect((train4.x+20),train4.y,290,140,25);
+      stroke(0);
+      fill(0);
+
+      let station4="- Wagah Border -\n- Pakistan -\nSeptember 20, 1947\nLast Train Stop\n- finally reached Lahore\n- immediately felt a sense of peace";
+      textAlign(CENTER);
+      textFont(comforta);
+      textLeading(20);
+      text(station4,train4.x+165,train4.y+20);
+    }
+
+
+    noStroke();
+    let train5=pakistanmap.latLngToPixel(28.6561592,77.2410203); //Red Fort
+    if ((mouseX>train5.x-(dix/2))&& (mouseX<train5.x+(dix/2)) && (mouseY>train5.y-(diy/2)) && (mouseY<train5.y+(diy/2))){//boolean if mousex and mousey is in each cardd
+      fill(217,120,85);
+      rect((train5.x+20),train5.y,290,100,25);
+      stroke(0);
+      fill(0);
+
+      let station5="- Red Fort -\n- India -\nSeptember 19, 1947\n-seen on the way to New Delhi Station";
+      textAlign(CENTER);
+      textFont(comforta);
+      textLeading(20);
+      text(station5,train5.x+165,train5.y+20);
+    }
+    let train6=pakistanmap.latLngToPixel(28.6506792,77.2334421); //Jama Masjid
+
+    if ((mouseX>train6.x-(dix/2))&& (mouseX<train6.x+(dix/2)) && (mouseY>train6.y-(diy/2)) && (mouseY<train6.y+(diy/2))){//boolean if mousex and mousey is in each cardd
+      fill(217,120,85);
+      rect((train6.x+20),train6.y,290,100,25);
+      stroke(0);
+      fill(0);
+
+      let station6="- Jama Masjid -\n- India -\nSeptember 19, 1947\n-seen on the way to New Delhi Station";
+      textAlign(CENTER);
+      textFont(comforta);
+      textLeading(20);
+      text(station6,train6.x+165,train6.y+20);
+    }
+
+    drawSquare(); 
+    }
+  pop();
+
   if (scene==1){
     background(255);//green background
     //position to be in top middle
+    // song.play();
     drawSquare(); //function to make frame
     textprocessed();//function for text
     soundWave(); //function for sound wave
@@ -289,10 +708,7 @@ function draw() {
       text(station6,train6.x+165,train6.y+20);
     }
 
-
-    
     drawSquare();  
-
 
   } else if (scene==4){
     background(0);
@@ -424,7 +840,7 @@ function movingPhrases(){
       phrase++;
   }
   if (phrase==1){
-    song.play();
+    // song.play();
   }
 }
 
